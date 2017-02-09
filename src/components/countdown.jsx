e2ee353fb290ba93004cc039a7dc114c20846271
Vadim Postanovkin <v.postanovkin@gmail.com>
2017-02-09 19:22:06 +0300

    Update with page-title

--- src/components/countdown.jsx
+++ src/components/countdown.jsx
@@ -69,6 +69,7 @@ export class Countdown extends React.Component {
         };
         return(
             <div>
+                <h1 className="page-title">Countdown</h1>
                 <Clock totalSeconds={count}/>
                 {renderControls()}
             </div>     
